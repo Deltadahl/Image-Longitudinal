04602b3614b2bc346b89bb3008e0984db034d3d1
Simon Carlson <ccsimon123@gmail.com>
2024-03-08 18:18:50 +0000

    updated scripts to train with images - still some bug in result.

--- src/synthetic_model_test_generate_data_diffusion.jl
+++ src/synthetic_model_test_generate_data_diffusion.jl
@@ -47,35 +47,82 @@ function main()
 
     # Pair training and test data with their respective labels
     imgs_to_consider = 100_000
+    # imgs_to_consider = 200
     η_approx = η_matrix[:,1:imgs_to_consider]
-    test_data = (lvs_matrix[:,1:imgs_to_consider], η_approx)
+    test_data = (collect(1:imgs_to_consider), η_approx)
+    # test_data = (lvs_matrix[:,1:imgs_to_consider], η_approx)
 
     test_dataloader = DataLoader(test_data, batchsize=batch_size, shuffle=false)
     test_dataloader = test_dataloader |> DEVICE
 
     model_name = "noise_$(var_noise)_save_nr_$(load_model_nr).jld2"
-    model_name_vae = "save_nr_$(load_model_nr_vae).jld2"
+    # model_name_vae = "save_nr_$(load_model_nr_vae).jld2"
 
     synthetic_model = load("synthetic_saved_models/" * model_name, "model")
-    vae = load("saved_models/" * model_name_vae, "vae")
+    # vae = load("saved_models/" * model_name_vae, "vae")
 
     print_model(synthetic_model)
 
     synthetic_model.to_random_effects = synthetic_model.to_random_effects |> DEVICE
     synthetic_model = synthetic_model |> DEVICE
-    vae.decoder = vae.decoder |> DEVICE
-    vae = vae |> DEVICE # Can probobly be removed.
+    # vae.decoder = vae.decoder |> DEVICE
+    # vae = vae |> DEVICE # Can probobly be removed.
+
+    # Helper function to extract the numerical part of the filename
+    function extract_number(filename::String)
+        # Attempt to match the numerical part of the filename
+        match_result = match(r"image_(\d+)\.jpeg", filename)
+        if match_result !== nothing
+            # If a match is found, parse the number and return it
+            return parse(Int, match_result.captures[1])
+        else
+            # If no match is found, return a default value
+            return -1
+        end
+    end
+
+    # Adjusted function to collect and then sort all images across subfolders
+    function load_images_and_sort_globally(image_subfolders)
+        images = []
+        for subfolder in image_subfolders
+            image_files = readdir(subfolder, join=true)
+            append!(images, image_files)
+        end
+        # Sort all collected image paths globally based on the extracted number
+        sort!(images, by=filename -> extract_number(String(split(filename, '/')[end])))
+        images
+    end
+
+    image_data_dir = "saved_eta_and_lv_data/json/output/"
+    image_subfolders = readdir(image_data_dir, join=true)
+
+    images = load_images_and_sort_globally(image_subfolders) # Ensure this function loads images or paths
+
+    function preprocess_image(path)
+        img = load(path) # Load the image from path
+        img_resized = imresize(img, (224, 224)) # Resize the image
+        return img_resized
+    end
 
     η_pred = zeros(Float32, 3, imgs_to_consider)
-    imgs_list = zeros(Float32, 224, 224, 1, imgs_to_consider)
+    # imgs_list = zeros(Float32, 224, 224, 1, imgs_to_consider)
     for (idx, (x_batch_test, y_batch_test)) in enumerate(test_dataloader)
+        if idx % 50 == 0
+            println("idx: ", idx)
+        #    break
+        end
 
-        imgs = vae.decoder(x_batch_test)
+        batch_images = [CUDA.cu(Float32.(preprocess_image(images[x]))) for x in Array(x_batch_test)]
+        # Stack images into a 4D array and add channel dimension
+        stacked_images = cat(batch_images..., dims=4)
+        imgs = reshape(stacked_images, size(stacked_images, 1), size(stacked_images, 2), 1, size(stacked_images, 4))
+
+        # imgs = vae.decoder(x_batch_test)
         y_approx = synthetic_model(imgs)
         y_approx = y_approx |> cpu
-        imgs = imgs |> cpu
+        # imgs = imgs |> cpu
         η_pred[:, (idx-1)*batch_size+1:min(idx*batch_size, imgs_to_consider)] = y_approx
-        imgs_list[:, :, :, (idx-1)*batch_size+1:min(idx*batch_size, imgs_to_consider)] = imgs
+        # imgs_list[:, :, :, (idx-1)*batch_size+1:min(idx*batch_size, imgs_to_consider)] = imgs
     end
 
     @show mean((η_pred .- η_approx).^2)
@@ -97,12 +144,6 @@ function main()
 
     filename = "data/synthetic/noise_$(var_noise)_eta_pred.jld2"
     save(filename, "η_pred", η_pred)
-
-    # IF YOU WANT TO SAVE THE IMAGES, USE THIS CODE
-    # for i = 1:imgs_to_consider
-    #     img = Images.colorview(Gray, imgs_list[:,:,1,i])
-    #     save("data/synthetic/imgs/img_$(i).png", img)
-    # end
 
     # Function to calculate MSE on bootstrap sample
     mse(x::Matrix) = mean((x[:,1] .- x[:,2]).^2)
---
+++ src/synthetic_train_model_old.jl
@@ -0,0 +1,128 @@
+# synthetic_train_model.jl
+using JLD2, FileIO
+using Flux
+using Flux.Optimise
+using Flux: params
+using Glob
+using Printf
+using CUDA
+using Flux.Data: DataLoader
+include("synthetic_model.jl")
+include("synthetic_utils_train.jl")
+include("data_manipulation/plot_losses_synthetic.jl")
+include("VAE.jl")
+
+function main()
+    var_noise = 0.0
+    epochs = 86
+    load_model_nr = 0
+    load_model_nr_vae = 526
+    try_nr = 19
+
+    evaluate_interval = 16000 * 6
+    batch_size = 16
+
+    # filepath = "data/synthetic/eta_and_lv_data_2_1M.jld2"
+    # filepath = "data/synthetic/noise_$(var_noise)_eta_approx_and_lv_data_1000k.jld2"
+    filepath = "saved_eta_and_lv_data/noise_$(var_noise)_eta_approx_and_lv_data_1000k.jld2"
+    η_matrix = load(filepath, "η_approx")
+    # η_matrix = Float32.(hcat([t.η for t in η_approx]...))
+    η_matrix = Float32.(η_matrix)
+    lvs_matrix = load(filepath, "lvs_matrix")
+
+    # Just to check that the MSE with the selected dim is much lower than with the other dims
+    # mse lvs_matrix 92, 111 and 50 vs η_matrix
+    @show mean((lvs_matrix[92, :] .- η_matrix[1,:]).^2)
+    @show mean((lvs_matrix[111, :] .- η_matrix[1,:]).^2)
+    @show mean((lvs_matrix[50, :] .- η_matrix[1,:]).^2)
+    println()
+    @show mean((lvs_matrix[92, :] .- η_matrix[2,:]).^2)
+    @show mean((lvs_matrix[111, :] .- η_matrix[2,:]).^2)
+    @show mean((lvs_matrix[50, :] .- η_matrix[2,:]).^2)
+    println()
+    @show mean((lvs_matrix[92, :] .- η_matrix[3,:]).^2)
+    @show mean((lvs_matrix[111, :] .- η_matrix[3,:]).^2)
+    @show mean((lvs_matrix[50, :] .- η_matrix[3,:]).^2)
+
+    num_samples = size(lvs_matrix, 2)
+    split_idx = Int(floor(0.95 * num_samples))
+    # Split the data into training and test sets
+    lvs_train = lvs_matrix[:, 1:split_idx]
+    lvs_test = lvs_matrix[:, split_idx+1:end]
+    η_train = η_matrix[:, 1:split_idx]
+    η_test = η_matrix[:, split_idx+1:end]
+
+    # Pair training and test data with their respective labels
+    train_data = (lvs_train, η_train)
+    test_data = (lvs_test, η_test)
+
+    train_dataloader = DataLoader(train_data, batchsize=batch_size, shuffle=true)
+    test_dataloader = DataLoader(test_data, batchsize=batch_size, shuffle=false)
+
+    train_dataloader = train_dataloader |> DEVICE
+    test_dataloader = test_dataloader |> DEVICE
+
+    model_name = "save_nr_$(load_model_nr).jld2"
+    model_name_vae = "save_nr_$(load_model_nr_vae).jld2"
+
+    if load_model_nr > 0
+        synthetic_model = load("synthetic_saved_models/" * model_name, "model")
+    else
+        to_random_effects = create_synthetic_model()
+        synthetic_model = SyntheticModel(to_random_effects)
+    end
+    vae = load("saved_models/" * model_name_vae, "vae")
+
+    print_model(synthetic_model)
+
+    synthetic_model.to_random_effects = synthetic_model.to_random_effects |> DEVICE
+    synthetic_model = synthetic_model |> DEVICE
+    vae.decoder = vae.decoder |> DEVICE
+    vae = vae |> DEVICE # Probably not needed.
+
+    ps = params(synthetic_model)
+    opt = ADAM(0.001)
+
+    start_time = time()
+    save_nr = 1
+    if load_model_nr > 0
+        save_nr += load_model_nr
+    end
+
+    for epoch in 1:epochs
+        println("Epoch: $epoch/$epochs")
+        loss_saver = LossSaverSynthetic(0.0f0, 0.0f0)
+        for (batch_nr, (x_batch, y_batch)) in enumerate(train_dataloader)
+            images = vae.decoder(x_batch)
+
+            println("size(images): ", size(images))
+            println("typeof(images): ", typeof(images))
+
+            println("size(y_batch): ", size(y_batch))
+            println("typeof(y_batch): ", typeof(y_batch))
+
+
+
+            train!(synthetic_model, images, y_batch, opt, ps, loss_saver)
+
+            if batch_nr * batch_size % evaluate_interval == 0
+                loss_saver_test = LossSaverSynthetic(0.0f0, 0.0f0)
+                for (x_batch_test, y_batch_test) in test_dataloader
+                    images_test = vae.decoder(x_batch_test)
+
+                    loss(synthetic_model, images_test, y_batch_test, loss_saver_test)
+                end
+
+                print_and_save(start_time, loss_saver, loss_saver_test, try_nr, save_nr)
+                loss_saver = LossSaverSynthetic(0.0f0, 0.0f0)
+
+                save_model(save_nr, synthetic_model)
+                plot_losses(try_nr, evaluate_interval, split_idx)
+                save_nr += 1
+            end
+        end
+    end
+    return nothing
+end
+
+main()
